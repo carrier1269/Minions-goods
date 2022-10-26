5e759de6b207b83ee76c57c5d12f2da37ec95ef5
carrier1269 <carrier1269@naver.com>
2022-10-27 03:54:46 +0900

    #App Inventor APK file

---
+++ minions/src/appinventor/ai_chlrkdls1269/minions/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"219","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"30","AppName":"\ubbf8\ub2c8\uc5b8\uc988","BackgroundImage":"minions(2).png","Icon":"7b03b513235e20b5bce4ac0c211b672d.png","ScreenOrientation":"portrait","Title":"Screen1","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"\ubc84\ud2bc1","$Type":"Button","$Version":"7","BackgroundColor":"&H00FFFFFF","Height":"-2","Width":"-2","Uuid":"-1905030557"}]}}
+|#
